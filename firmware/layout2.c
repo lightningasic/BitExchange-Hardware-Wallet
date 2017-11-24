cf9d06994ec2fefdcc09efa52f9194d9e9e7e735
Wenfeng Wang <kalot.wang@gmail.com>
2017-11-24 14:13:49 +0800

    Fix layoutAddress and other issue within layout module

--- firmware/layout2.c
+++ firmware/layout2.c
@@ -194,9 +194,8 @@ void layoutConfirmTx(const CoinType *coin, uint64_t amount_out, uint64_t amount_
 	}
 }
 
-void layoutFeeOverThreshold(const CoinType *coin, uint64_t fee, uint32_t kb)
+void layoutFeeOverThreshold(const CoinType *coin, uint64_t fee)
 {
-	(void)kb;
 	const char *str_out = str_amount(fee, coin->has_coin_shortcut ? coin->coin_shortcut : NULL, buf_out, sizeof(buf_out));
 	switch (storage_getLang()) {
 		case CHINESE :
@@ -274,7 +273,7 @@ void layoutVerifyAddress(const char *address)
 		case CHINESE :
 			layoutZhDialogSwipe(DIALOG_ICON_QUESTION, "取消", "确认",
 					"地址确认#?#",
-					"消息签名者#?#"
+					"消息签名者#?#",
 					str[0], str[1], str[2]);
 			break;
 		default :
@@ -355,35 +354,66 @@ void layoutDecryptMessage(const uint8_t *msg, uint32_t len, const char *address)
 	}
 }
 
-void layoutAddress(const char *address)
+void layoutAddress(const char *address, const char *desc, bool qrcode)
 {
-	oledSwipeLeft();
+	if (layoutLast != layoutAddress) {
+		oledSwipeLeft();
+	} else {
+		oledClear();
+	}
 	layoutLast = layoutAddress;
 
-	static unsigned char bitdata[QR_MAX_BITDATA];
-	int a, i, j;
-	int side = qr_encode(QR_LEVEL_M, 0, address, 0, bitdata);
+	if (qrcode) {
+		static unsigned char bitdata[QR_MAX_BITDATA];
+		int side = qr_encode(QR_LEVEL_M, 0, address, 0, bitdata);
 
-	if (side > 0 && side <= 29) {
-		oledInvert(0, 0, (side + 2) * 2, (side + 2) * 2);
-		for (i = 0; i < side; i++) {
-			for (j = 0; j< side; j++) {
-				a = j * side + i;
-				if (bitdata[a / 8] & (1 << (7 - a % 8))) {
-					oledClearPixel(2 + i * 2, 2 + j * 2);
-					oledClearPixel(3 + i * 2, 2 + j * 2);
-					oledClearPixel(2 + i * 2, 3 + j * 2);
-					oledClearPixel(3 + i * 2, 3 + j * 2);
+		if (side > 0 && side <= 29) {
+			int i,j;
+			oledInvert(0, 0, (side + 2) * 2, (side + 2) * 2);
+			for (i = 0; i < side; i++) {
+				for (j = 0; j< side; j++) {
+					int a = j * side + i;
+					if (bitdata[a / 8] & (1 << (7 - a % 8))) {
+						oledClearPixel(2 + i * 2, 2 + j * 2);
+						oledClearPixel(3 + i * 2, 2 + j * 2);
+						oledClearPixel(2 + i * 2, 3 + j * 2);
+						oledClearPixel(3 + i * 2, 3 + j * 2);
+					}
+				}
+			}
+		} else if (side > 0 && side <= 60) {
+			int i,j;
+			oledInvert(0, 0, (side + 3), (side + 3));
+			for (i = 0; i < side; i++) {
+				for (j = 0; j< side; j++) {
+					int a = j * side + i;
+					if (bitdata[a / 8] & (1 << (7 - a % 8))) {
+						oledClearPixel(2 + i, 2 + j);
+					}
 				}
 			}
 		}
+	} else {
+		uint32_t addrlen = strlen(address);
+		uint32_t rowlen = addrlen / 2;
+		int i;
+		if (addrlen % 2) {
+			rowlen++;
+		}
+		const char **str = split_message((const uint8_t *)address, addrlen, rowlen);
+		if (desc) {
+			oledDrawString(0, 0 * 9, desc);
+		}
+		for (i = 0; i < 4; i++) {
+			oledDrawString(0, (i + 1) * 9 + 4, str[i]);
+		}
 	}
 
-	const char **str = split_message((const uint8_t *)address, strlen(address), 9);
-	oledDrawString(68, 0 * 9, str[0]);
-	oledDrawString(68, 1 * 9, str[1]);
-	oledDrawString(68, 2 * 9, str[2]);
-	oledDrawString(68, 3 * 9, str[3]);
+	if (!qrcode) {
+		static const char *btnNo = "QR Code";
+		oledDrawString(2, OLED_HEIGHT - 8, btnNo);
+		oledInvert(0, OLED_HEIGHT - 9, oledStringWidth(btnNo) + 3, OLED_HEIGHT - 1);
+	}
 
 	static const char *btnYes = "Continue";
 	static const char *btnZhYes = "继续";
@@ -396,7 +426,7 @@ void layoutAddress(const char *address)
 		default :
 			oledDrawString(OLED_WIDTH - fontCharWidth('\x06') - 1, OLED_HEIGHT - 8, "\x06");
 			oledDrawString(OLED_WIDTH - oledStringWidth(btnYes) - fontCharWidth('\x06') - 3, OLED_HEIGHT - 8, btnYes);
-			oledInvert(OLED_WIDTH - oledStringWidth(btnYes) - fontCharWidth('\x06') - 4, OLED_HEIGHT - 9, OLED_WIDTH - 1, OLED_HEIGHT - 1); 
+			oledInvert(OLED_WIDTH - oledStringWidth(btnYes) - fontCharWidth('\x06') - 4, OLED_HEIGHT - 9, OLED_WIDTH - 1, OLED_HEIGHT - 1);
 			break;
 	}
 
